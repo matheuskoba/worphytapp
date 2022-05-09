173c80bafa2b336bcdc9c4b87bff102861adac88
matheuskoba <matheuskoba2021@icloud.com>
2022-05-09 09:20:32 -0300

    update

--- src/screens/Personal/index.js
+++ src/screens/Personal/index.js
@@ -1,4 +1,5 @@
 import React,{useState, useEffect} from "react";
+import { Text } from "react-native";
 import { useNavigation, useRoute } from "@react-navigation/native";
 import Swiper from 'react-native-swiper';
 
@@ -6,6 +7,8 @@ import Stars from '../../components/Stars';
 
 import FavoriteIcon from '../../assets/favorite.svg';
 import BackIcon from '../../assets/back.svg';
+import NavPrevIcon from '../../assets/nav_prev.svg';
+import NavNextIcon from '../../assets/nav_next.svg';
 
 import { 
     Container,
@@ -36,6 +39,10 @@ import {
     ServiceChooseButton,
 
     TestimonialArea,
+    TestimonialItem,
+    TestimonialInfo,
+    TestimonialName,
+    TestimonialBody,
  } from './styles';
 
 import Api from '../../Api';
@@ -128,10 +135,29 @@ export default () => {
                             })}
                         </ServiceArea>
                     }
-                   
-                    <TestimonialArea>
+                   {userInfo.testimonials && userInfo.testimonials.lenght > 0 &&
+                        <TestimonialArea>
+                            <Swiper
+                                style={{height: 110}}
+                                showsPagination={false}
+                                showsButtons={true}
+                                prevButton={<NavPrevIcon width="35" heigth="35" fill="#000" />}
+                                nextButton={<NavNextIcon width="35" heigth="35" fill="#000" />}
+                            >
+                                {userInfo.testimonials.map((item,key) => (
+                                    <TestimonialItem key={key}>
+                                        <TestimonialInfo>
+                                            <TestimonialName>{item.name}</TestimonialName>
+                                            <Stars stars={item.rate} showNumber={true} />
+                                        </TestimonialInfo>
+                                        <TestimonialBody>{item.body}</TestimonialBody>
+                                    </TestimonialItem>
+                                ))}
 
-                    </TestimonialArea>
+                            </Swiper>
+
+                        </TestimonialArea>
+                    }
                 </PageBody>
             </Scroller>
             <BackButton onPress={handleBackButton}>
--- src/screens/Personal/styles.js
+++ src/screens/Personal/styles.js
@@ -115,7 +115,34 @@ export const ServiceChooseBtnText = styled.Text`
     color: #fff;
 `;
 
-export const TestimonialArea = styled.View``;
+export const TestimonialArea = styled.View`
+    margin-top: 30px;
+    margin-bottom: 50px;
+`;
+export const TestimonialItem = styled.View`
+    background-color: #000;
+    padding: 15px;
+    border-radius: 10px;
+    height: 110px;
+    justify-content: center;
+    margin-left: 50px;
+    margin-right: 50px;
+`;
+export const TestimonialInfo = styled.View`
+    flex-direction: row;
+    justify-content: space-between;
+    margin-bottom: 5px;
+`;
+export const TestimonialName = styled.Text`
+    color: #fff;
+    font-size: 14px;
+    font-weight: bold;
+`;
+export const TestimonialBody = styled.Text`
+    color: #fff;
+    font-size: 13px;
+`;
+
 export const BackButton = styled.TouchableOpacity`
     position: absolute;
     left: 0;
